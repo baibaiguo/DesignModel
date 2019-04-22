3a65ee36b0fb4edd219ac918d6cfb23b1dd26207
baibaiguo <baibaiguo@gmail.com>
2019-04-22 17:17:13 +0800

    Initial commit

---
+++ src/com/baiii/singleton/SingletonDemo1.java
@@ -0,0 +1,11 @@
+package com.baiii.singleton;
+
+public class SingletonDemo1 {
+
+    private static SingletonDemo1 instance = new SingletonDemo1();
+    private SingletonDemo1(){}
+
+    public static SingletonDemo1 getInstance(){
+        return instance;
+    }
+}
